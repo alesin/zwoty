4f253f3db7650b95ec4109fc6afe955bfee46f6e
alesin <alesintipler@gmail.com>
2020-08-09 06:21:37 -0400

    add: fitbit API routes (used after auth routes)

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/fitbit', require('./fitbit'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
