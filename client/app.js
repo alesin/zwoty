c40d4da640ac21a0444f2534f39880172e5a6912
alesin <alesintipler@gmail.com>
2020-08-09 06:11:00 -0400

    remove old NavBar import

--- client/app.js
+++ client/app.js
@@ -3,9 +3,6 @@ import React from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
 
-// import {MiniDrawer} from './components/NavbarWithPersistentDrawer'
-// import {NavbarWithPersistentDrawer, NavbarWithTempDrawer} from './components'
-
 const App = () => {
   return (
     <div>
