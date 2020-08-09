f2bd39ff673866c56de1167727a909f8444f5db6
alesin <alesintipler@gmail.com>
2020-08-09 16:25:05 -0400

    split route protection for authentication & authorization

--- server/api/utils/routeProtect.js
+++ server/api/utils/routeProtect.js
@@ -1,4 +1,4 @@
-const isLoggedIn = (req, res, next) => {
+const isLegit = (req, res, next) => {
   if (req.user === undefined || req.user.id !== Number(req.params.userId)) {
     const error = new Error('illegal action')
     error.status = 401
@@ -6,5 +6,9 @@ const isLoggedIn = (req, res, next) => {
   }
   next()
 }
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/')
+}
 
-module.exports = isLoggedIn
+module.exports = {isLoggedIn, isLegit}
