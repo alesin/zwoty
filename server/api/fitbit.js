3f9a298ebcf03758cf11033aaa2fd1b3805a7fb6
alesin <alesintipler@gmail.com>
2020-08-09 06:27:15 -0400

    IN PROGRESS: utilize many User instance methods

---
+++ server/api/fitbit.js
@@ -0,0 +1,56 @@
+const router = require('express').Router()
+const {User} = require('../db/models')
+const axios = require('axios')
+const isLoggedIn = require('../api/utils/routeProtect')
+
+// const isLoggedIn = (req, res, next) => {
+//     if (req.user === undefined || req.user.id !== Number(req.params.userId)) {
+//     const error = new Error('illegal action')
+//     error.status = 401
+//     return next(error)
+//     }
+//     next()
+// }
+
+router.get('/:userId', isLoggedIn, async (req, res, next) => {
+  try {
+    // const theUser = await User.findByPk(req.params.userId, {
+    //     attributes: ['id']
+    // })
+    const theUser = await User.findOne({
+      where: {
+        userId: req.params.userId
+      },
+      // ^^ explicitly select only the id and fitbitId fields - even though users' passwords are encrypted, it won't help if we just send everything to anyone who asks!
+      attributes: ['id', 'fitbitId']
+    })
+    res.json(theUser)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:userId/profile', isLoggedIn, async (req, res, next) => {
+  try {
+    // const theUser = await User.findByPk(req.params.userId, {
+    //     attributes: ['id']
+    // })
+    let url = User.buildURL('-', 'profile.json')
+    const theUser = await User.findOne({
+      where: {
+        // userId: req.params.userId
+        id: req.params.userId
+      },
+      attributes: ['id', 'fitbitId']
+    })
+
+    // let url = theUser.buildURL('-', 'profile.json')
+    let userProfile = await theUser.userRequest(url)
+
+    res.json(userProfile)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
