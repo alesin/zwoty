e730e6e36c2add1bba9116b574965bbb26663a83
alesin <alesintipler@gmail.com>
2020-08-09 05:05:19 -0400

    minor adjustment: change authentication redirect URL

--- server/auth/google.js
+++ server/auth/google.js
@@ -52,7 +52,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   router.get(
     '/verify',
     passport.authenticate('google', {
-      successRedirect: '/home',
+      successRedirect: '/dash',
       failureRedirect: '/login'
     })
   )
