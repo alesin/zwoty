b4c864ee5ac3357af2a9c54b1913c07df47284dd
alesin <alesintipler@gmail.com>
2020-08-09 06:16:55 -0400

    add: connectStream & getFitbitProfile (the latter may need to go)

--- client/store/user.js
+++ client/store/user.js
@@ -2,24 +2,28 @@ import axios from 'axios'
 import history from '../history'
 
 /**
- * ACTION TYPES
+ //* ACTION TYPES
  */
 const GET_USER = 'GET_USER'
+const CONNECTED_STREAM = 'CONNECTED_STREAM'
+const GOT_FITBIT_PROFILE = 'GET_FITBIT_PROFILE'
 const REMOVE_USER = 'REMOVE_USER'
 
 /**
- * INITIAL STATE
+ //* INITIAL STATE
  */
 const defaultUser = {}
 
 /**
- * ACTION CREATORS
+ //* ACTION CREATORS
  */
 const getUser = user => ({type: GET_USER, user})
+const connectedStream = stream => ({type: CONNECTED_STREAM, stream})
+const gotFitbitProfile = profile => ({type: GOT_FITBIT_PROFILE, profile})
 const removeUser = () => ({type: REMOVE_USER})
 
 /**
- * THUNK CREATORS
+ //* THUNK CREATORS
  */
 export const me = () => async dispatch => {
   try {
@@ -29,7 +33,6 @@ export const me = () => async dispatch => {
     console.error(err)
   }
 }
-
 export const auth = (email, password, method) => async dispatch => {
   let res
   try {
@@ -40,12 +43,11 @@ export const auth = (email, password, method) => async dispatch => {
 
   try {
     dispatch(getUser(res.data))
-    history.push('/home')
+    history.push('/dash')
   } catch (dispatchOrHistoryErr) {
     console.error(dispatchOrHistoryErr)
   }
 }
-
 export const logout = () => async dispatch => {
   try {
     await axios.post('/auth/logout')
@@ -55,14 +57,41 @@ export const logout = () => async dispatch => {
     console.error(err)
   }
 }
+export const connectStream = (dataStream, userId) => async dispatch => {
+  try {
+    const {data} = await axios.get(`/auth/${dataStream}/success/${userId}`)
+    dispatch(connectedStream(data))
+  } catch (error) {
+    console.log(`Whoops, trouble connecting to ${dataStream}!`, error)
+  }
+}
+export const getFitbitProfile = userId => {
+  return async dispatch => {
+    try {
+      console.log('In Fitbit Profile THUNK ==>')
+      // ? Do we want to do a direct external API call here?
+      // const {data} = await axios.get('https://api.fitbit.com/1/user/-/profile.json')
+      const {data} = await axios.get(`/api/fitbit/${userId}/profile`)
+      console.log(data)
+      dispatch(gotFitbitProfile(data))
+    } catch (error) {
+      console.log('Whoops, trouble getting Fitbit profile!', error)
+    }
+  }
+}
 
 /**
- * REDUCER
+ //* REDUCER
  */
 export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
       return action.user
+    case CONNECTED_STREAM:
+      if (action.stream === 'fitbit') return {...state, fitbit: action.stream}
+      else return
+    case GOT_FITBIT_PROFILE:
+      return {...state, fitbit: {...state.fitbit, profile: action.profile}}
     case REMOVE_USER:
       return defaultUser
     default:
